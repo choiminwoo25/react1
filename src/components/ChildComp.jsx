c9f107b2dcc58bfd89359dbf46795ca97915d4be
choiminwoo25 <cmotwin1108@naver.com>
2026-04-01 11:37:38 +0900

    image 속성값 가져오기

--- src/components/ChildComp.jsx
+++ src/components/ChildComp.jsx
@@ -1,11 +1,11 @@
-import reactLogo from '../assets/react.svg'
+
 import App from '../App'
 
 
-export default function ChildComp ({alt,width,height}) {
+export default function ChildComp ({imageInfo,width,height}) {
   return(
     <>
-     <img className="button-icon" src={reactLogo} alt={alt} width={width} height={height}/>
+     <img src={imageInfo.src} alt={imageInfo.alt} width={width} height={height}/>
     </>
   )
 }
--- src/components/ParentComp.jsx
+++ src/components/ParentComp.jsx
@@ -1,12 +1,23 @@
 import Profile from "./profile"
 import MyTitle from "./MyTitle"
 import ChildComp from "./ChildComp"
+import reactLogo from "../assets/react.svg";
 
 
 export default function ParentComp() {
     return(
         <>
-         <ChildComp alt="React" width={100} height={100}/>
+         <ChildComp
+            imageInfo={
+                {
+                    src: reactLogo,
+                    alt: "React",
+                }
+            }
+           width={100} height={100}
+         
+         
+         />
 
         </>
     )
